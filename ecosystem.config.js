e3a2e751cd8c17fcd97fd944061e035a31bf3910
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-06-30 17:11:20 +0700

    deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "student_course",
+    script: "./src/server.js", // Adjust this path to where your server's entry point is
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: "dev",
+    },
+  }],
+};
